814da86585a598969bf87f8c2a799f155926fb72
oshenn <oskars.henrici@gmail.com>
2024-06-24 13:15:02 +0300

    Laravel Breeze pievienots projektam.

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\AircraftController;
 use App\Http\Controllers\CommentController;
-use App\Http\Controllers\EventController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AircraftController::class, 'index'])->name('aircraft.index');
-Route::get('/aircraft', [AircraftController::class, 'index'])->name('aircraft.index');
-Route::get('/aircraft/{aircraft}', [AircraftController::class, 'show'])->name('aircraft.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::get('/events', [EventController::class, 'index'])->name('events.index');
 Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
+Route::get('/aircraft', [AircraftController::class, 'index'])->name('aircraft.index');
+Route::get('/aircraft/{aircraft}', [AircraftController::class, 'show'])->name('aircraft.show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
